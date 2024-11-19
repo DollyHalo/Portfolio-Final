e8bb98c786b9a26d33fd8324c7fbb256fc0b94e0
HollySloat <157514488+DollyHalo@users.noreply.github.com>
2024-11-19 10:21:55 -0600

    added carousel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // JS scripts placed here
+$(document).ready(function(){
+    $('.carousel').slick({
+    
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      fade: true;
+    });
+  });
+
+
+
+  
